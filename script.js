320a3955a83e27b630b57bf3e7f425d2a7347144
Huzaifa <76128718+Huzaifa4472@users.noreply.github.com>
2023-08-07 11:53:53 +0500

    update

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-'use strict';
+// 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-  order:function(starterIndex, mainIndex){
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  }
-  
-}; 
-let {name, openingHours, categories} = restaurant;
-console.log(name, openingHours, categories);
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0,
+//       close: 24,
+//     },
+//   },
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+// };
+// let { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-let {name: restaurantName, openingHours: hour, categories: tags} = restaurant;
-console.log(restaurantName, hour, tags)
+// let { name: restaurantName, openingHours: hour, categories: tags } = restaurant;
+// console.log(restaurantName, hour, tags);
 
- // Default values //
+// // Default values //
 
-let {menu = [], starterMenu : starter = []} = restaurant;
-console.log(menu, starter)
+// let { menu = [], starterMenu: starter = [] } = restaurant;
+// console.log(menu, starter);
 
 // let arr = [2,3,4];
 // let a = arr[0];
@@ -49,8 +48,6 @@ console.log(menu, starter)
 // let [x,y,z] = arr;
 // console.log(x,y,z)
 // console.log(arr)
-
-
 
 // let [first, second] = restaurant.categories;
 // console.log(first, second)
@@ -87,3 +84,4 @@ console.log(menu, starter)
 
 // let [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p,q,r);
+// console.log('Hello');
